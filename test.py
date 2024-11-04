55d48d9870651e1e3f2847a4969af402f66115ad
davnish <nischal.singh38@gmail.com>
2024-11-04 05:39:33 +0530

    added dataset, runs

--- test.py
+++ test.py
@@ -12,5 +12,5 @@ for i in glob.glob("preprocessing_data/8bit/patched_full/*.tif"):
     file_name = os.path.splitext(os.path.split(i)[1])[0]
     results = model(i)
     results[0].show()
-    print(results[0].save_txt(f'results/{file_name}.txt'))
+    print(results[0].save_txt(f'results/txt/{file_name}.txt'))
     
